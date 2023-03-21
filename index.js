a47e56584bb969fe446fd7486978ba8d2f5f3ea4
laura-paullette <laura.paullette@student.moringaschool.com>
2023-03-21 18:53:19 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Write your solution in this file!
+var customerName = "bob";
+function upperCaseCustomerName(){
+    return bob.toUpperCase;
+}
+function upperCaseCustomerName(){
+    return customerName = "BOB";
+}
+function setBestCustomer(){
+    return bestCustomer = "not bob";
+}
+
+function overwriteBestCustomer(){
+    return bestCustomer = "maybe bob";
+}
+const leastFavoriteCustomer = "ted";
+function changeLeastFavoriteCustomer(){
+    leastFavoriteCustomer = "ted"
+}
